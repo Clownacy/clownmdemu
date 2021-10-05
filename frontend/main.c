6d26ce6e99e39f990e5ba16b25253920a7bf8277
Clownacy <Clownacy@users.noreply.github.com>
2021-10-05 19:41:53 +0100

    Improve the frontend
    Now has V-sync (obviously needs to be disabled if the monitor's
    refresh rate is lower than 60) and scales the framebuffer using the
    GPU, which should save some CPU.

--- frontend/main.c
+++ frontend/main.c
@@ -9,6 +9,11 @@
 #include "clownmdemu.h"
 
 static SDL_Window *window;
+static SDL_Renderer *renderer;
+static SDL_Texture *framebuffer_texture;
+
+static unsigned int current_screen_width;
+static unsigned int current_screen_height;
 
 static struct
 {
@@ -60,20 +65,23 @@ static void LoadFileToBuffer(const char *filename, unsigned char **file_buffer, 
 
 static void ScanlineRenderedCallback(unsigned short scanline, void *pixels, unsigned short screen_width, unsigned short screen_height)
 {
-	SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormatFrom(pixels, screen_width, 1, 0, screen_width * 3, SDL_PIXELFORMAT_RGB24);
+	// Upload scanline to texture
+	void *texture_pixels;
+	int texture_pitch;
 
-	if (surface != NULL)
+	if (SDL_LockTexture(framebuffer_texture, &(SDL_Rect){.x = 0, .y = scanline, .w = screen_width, .h = 1}, &texture_pixels, &texture_pitch) < 0)
 	{
-		SDL_Surface *window_surface = SDL_GetWindowSurface(window);
-
-		if (window_surface != NULL)
-		{
-			SDL_Rect destination_rect = {.x = 0, .y = scanline * window_surface->h / screen_height, .w = window_surface->w, .h = window_surface->h / screen_height};
-			SDL_BlitScaled(surface, NULL, window_surface, &destination_rect);
-		}
-
-		SDL_FreeSurface(surface);
+		PrintError("SDL_LockTexture failed with the following message - '%s'", SDL_GetError());
 	}
+	else
+	{
+		memcpy(texture_pixels, pixels, screen_width * 3);
+
+		SDL_UnlockTexture(framebuffer_texture);
+	}
+
+	current_screen_width = screen_width;
+	current_screen_height = screen_height;
 }
 
 static unsigned char ReadInputCallback(unsigned char button_id)
@@ -131,96 +139,144 @@ int main(int argc, char **argv)
 			}
 			else
 			{
-				void *clownmdemu_state = malloc(ClownMDEmu_GetStateSize());
+				renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 
-				if (clownmdemu_state == NULL)
+				if (renderer == NULL)
 				{
-					PrintError("Could not allocate memory for the ClownMDEmu state");
+					PrintError("SDL_CreateRenderer failed with the following message - '%s'", SDL_GetError());
 				}
 				else
 				{
-					ClownMDEmu_Init(clownmdemu_state);
+					framebuffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, 320, 240);
 
-					unsigned char *file_buffer;
-					size_t file_size;
-					LoadFileToBuffer(argv[1], &file_buffer, &file_size);
-
-					if (file_buffer == NULL)
+					if (framebuffer_texture == NULL)
 					{
-						PrintError("Could not load the ROM");
+						PrintError("SDL_CreateTexture failed with the following message - '%s'", SDL_GetError());
 					}
 					else
 					{
-						ClownMDEmu_UpdateROM(clownmdemu_state, file_buffer, file_size);
-						free(file_buffer);
+						void *clownmdemu_state = malloc(ClownMDEmu_GetStateSize());
 
-						ClownMDEmu_Reset(clownmdemu_state);
-
-						bool quit = false;
-
-						while (!quit)
+						if (clownmdemu_state == NULL)
 						{
-							SDL_Event event;
-							while (SDL_PollEvent(&event))
+							PrintError("Could not allocate memory for the ClownMDEmu state");
+						}
+						else
+						{
+							ClownMDEmu_Init(clownmdemu_state);
+
+							unsigned char *file_buffer;
+							size_t file_size;
+							LoadFileToBuffer(argv[1], &file_buffer, &file_size);
+
+							if (file_buffer == NULL)
 							{
-								switch (event.type)
+								PrintError("Could not load the ROM");
+							}
+							else
+							{
+								ClownMDEmu_UpdateROM(clownmdemu_state, file_buffer, file_size);
+								free(file_buffer);
+
+								ClownMDEmu_Reset(clownmdemu_state);
+
+								bool quit = false;
+
+								while (!quit)
 								{
-								case SDL_QUIT:
-									quit = true;
-									break;
-
-								case SDL_KEYDOWN:
-								case SDL_KEYUP:
-									bool pressed = event.type == SDL_KEYDOWN;
-
-									switch (event.key.keysym.scancode)
+									// Process events
+									SDL_Event event;
+									while (SDL_PollEvent(&event))
 									{
-										#define DO_KEY(state, code) case code: state = pressed; break;
-										DO_KEY(input.up,    SDL_SCANCODE_W)
-										DO_KEY(input.down,  SDL_SCANCODE_S)
-										DO_KEY(input.left,  SDL_SCANCODE_A)
-										DO_KEY(input.right, SDL_SCANCODE_D)
-										DO_KEY(input.a,     SDL_SCANCODE_O)
-										DO_KEY(input.b,     SDL_SCANCODE_P)
-										DO_KEY(input.c,     SDL_SCANCODE_LEFTBRACKET)
-										DO_KEY(input.start, SDL_SCANCODE_RETURN)
-										#undef DO_KEY
+										switch (event.type)
+										{
+										case SDL_QUIT:
+											quit = true;
+											break;
 
-									default:
-										break;
+										case SDL_KEYDOWN:
+										case SDL_KEYUP:
+											bool pressed = event.type == SDL_KEYDOWN;
+
+											switch (event.key.keysym.scancode)
+											{
+												#define DO_KEY(state, code) case code: state = pressed; break;
+												DO_KEY(input.up,    SDL_SCANCODE_W)
+												DO_KEY(input.down,  SDL_SCANCODE_S)
+												DO_KEY(input.left,  SDL_SCANCODE_A)
+												DO_KEY(input.right, SDL_SCANCODE_D)
+												DO_KEY(input.a,     SDL_SCANCODE_O)
+												DO_KEY(input.b,     SDL_SCANCODE_P)
+												DO_KEY(input.c,     SDL_SCANCODE_LEFTBRACKET)
+												DO_KEY(input.start, SDL_SCANCODE_RETURN)
+												#undef DO_KEY
+
+											default:
+												break;
+											}
+
+											break;
+										}
 									}
 
-									break;
+									// Run the emulator for a frame
+									ClownMDEmu_Iterate(clownmdemu_state, ScanlineRenderedCallback, ReadInputCallback);
+
+									// Correct the aspect ratio of the rendered frame
+									// (256x224 and 320x240 should be the same width, but 320x224 and 320x240 should be different heights - this matches the behaviour of a real Mega Drive)
+									int renderer_width, renderer_height;
+									SDL_GetRendererOutputSize(renderer, &renderer_width, &renderer_height);
+
+									SDL_Rect destination_rect;
+
+									if ((unsigned int)renderer_width > renderer_height * 320 / current_screen_height)
+									{
+										destination_rect.w = renderer_height * 320 / current_screen_height;
+										destination_rect.h = renderer_height;
+									}
+									else
+									{
+										destination_rect.w = renderer_width;
+										destination_rect.h = renderer_width * current_screen_height / 320;
+									}
+
+									destination_rect.x = (renderer_width - destination_rect.w) / 2;
+									destination_rect.y = (renderer_height - destination_rect.h) / 2;
+
+									// Draw the rendered frame to the screen
+									SDL_RenderClear(renderer);
+									SDL_RenderCopy(renderer, framebuffer_texture, &(SDL_Rect){.x = 0, .y = 0, .w = current_screen_width, .h = current_screen_height}, &destination_rect);
+									SDL_RenderPresent(renderer);
+
+									// Framerate manager - run at roughly 60FPS
+									static Uint32 next_time;
+									const Uint32 current_time = SDL_GetTicks();
+
+									if (current_time < next_time)
+										SDL_Delay(next_time - current_time);
+
+									next_time = SDL_GetTicks() + 1000 / 60;
 								}
 							}
 
-							ClownMDEmu_Iterate(clownmdemu_state, ScanlineRenderedCallback, ReadInputCallback);
+							FILE *state_file = fopen("state.bin", "wb");
 
-							SDL_UpdateWindowSurface(window);
+							if (state_file != NULL)
+							{
+								fwrite(clownmdemu_state, 1, ClownMDEmu_GetStateSize(), state_file);
 
-							// Framerate manager - run at roughly 60FPS
-							static Uint32 next_time;
-							const Uint32 current_time = SDL_GetTicks();
+								fclose(state_file);
+							}
 
-							if (current_time < next_time)
-								SDL_Delay(next_time - current_time);
+							ClownMDEmu_Deinit(clownmdemu_state);
 
-							next_time = SDL_GetTicks() + 1000 / 60;
+							free(clownmdemu_state);
 						}
+
+						SDL_DestroyTexture(framebuffer_texture);
 					}
 
-					FILE *state_file = fopen("state.bin", "wb");
-
-					if (state_file != NULL)
-					{
-						fwrite(clownmdemu_state, 1, ClownMDEmu_GetStateSize(), state_file);
-
-						fclose(state_file);
-					}
-
-					ClownMDEmu_Deinit(clownmdemu_state);
-
-					free(clownmdemu_state);
+					SDL_DestroyRenderer(renderer);
 				}
 
 				SDL_DestroyWindow(window);
