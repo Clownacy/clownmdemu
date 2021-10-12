76512b76a0b765b10c2e3a2edd685665ae059123
Clownacy <Clownacy@users.noreply.github.com>
2021-10-12 09:13:19 +0100

    Frontend: Don't dynamically allocate the state
    Since the state struct is exposed to the frontend, we can just make
    it global memory instead. This simplifies things and makes some more
    compiler optimisations possible, particularly with link-time
    optimisation.

--- frontend/main.c
+++ frontend/main.c
@@ -27,6 +27,9 @@ static struct
 	bool start;
 } inputs[2];
 
+static ClownMDEmu_State clownmdemu_state;
+static ClownMDEmu_State clownmdemu_save_state;
+
 static void PrintError(const char *fmt, ...)
 {
 	va_list args;
@@ -180,187 +183,174 @@ int main(int argc, char **argv)
 
 						framebuffer_texture_pitch /= sizeof(Uint32);
 
-						// Allocate ClownMDEmu state
-						const size_t clownmdemu_state_size = sizeof(ClownMDEmu_State);
-						ClownMDEmu_State *clownmdemu_state = malloc(clownmdemu_state_size * 2); // *2 because we're allocating room for a save state
+						ClownMDEmu_Init(&clownmdemu_state);
 
-						if (clownmdemu_state == NULL)
+						// For now, let's emulate a North American console
+						ClownMDEmu_SetJapanese(&clownmdemu_state, false);
+						ClownMDEmu_SetPAL(&clownmdemu_state, false);
+
+						// Load ROM to memory
+						unsigned char *file_buffer;
+						size_t file_size;
+						LoadFileToBuffer(argv[1], &file_buffer, &file_size);
+
+						if (file_buffer == NULL)
 						{
-							PrintError("Could not allocate memory for the ClownMDEmu state");
+							PrintError("Could not load the ROM");
 						}
 						else
 						{
-							ClownMDEmu_Init(clownmdemu_state);
+							ClownMDEmu_UpdateROM(&clownmdemu_state, file_buffer, file_size);
+							free(file_buffer);
 
-							// For now, let's emulate a North American console
-							ClownMDEmu_SetJapanese(clownmdemu_state, false);
-							ClownMDEmu_SetPAL(clownmdemu_state, false);
+							ClownMDEmu_Reset(&clownmdemu_state);
 
-							// Load ROM to memory
-							unsigned char *file_buffer;
-							size_t file_size;
-							LoadFileToBuffer(argv[1], &file_buffer, &file_size);
+							// Initialise save state
+							clownmdemu_save_state = clownmdemu_state;
 
-							if (file_buffer == NULL)
+							bool quit = false;
+
+							bool fast_forward = false;
+							unsigned int frameskip_counter = 0;
+
+							while (!quit)
 							{
-								PrintError("Could not load the ROM");
-							}
-							else
-							{
-								ClownMDEmu_UpdateROM(clownmdemu_state, file_buffer, file_size);
-								free(file_buffer);
-
-								ClownMDEmu_Reset(clownmdemu_state);
-
-								// Initialise save state
-								clownmdemu_state[1] = clownmdemu_state[0];
-
-								bool quit = false;
-
-								bool fast_forward = false;
-								unsigned int frameskip_counter = 0;
-
-								while (!quit)
+								// Process events
+								SDL_Event event;
+								while (SDL_PollEvent(&event))
 								{
-									// Process events
-									SDL_Event event;
-									while (SDL_PollEvent(&event))
+									switch (event.type)
 									{
-										switch (event.type)
-										{
-											case SDL_QUIT:
-												quit = true;
-												break;
+										case SDL_QUIT:
+											quit = true;
+											break;
 
-											case SDL_KEYDOWN:
-											case SDL_KEYUP:
+										case SDL_KEYDOWN:
+										case SDL_KEYUP:
+										{
+											bool pressed = event.type == SDL_KEYDOWN;
+
+											switch (event.key.keysym.scancode)
 											{
-												bool pressed = event.type == SDL_KEYDOWN;
+												#define DO_KEY(state, code) case code: state = pressed; break;
 
-												switch (event.key.keysym.scancode)
-												{
-													#define DO_KEY(state, code) case code: state = pressed; break;
+												DO_KEY(inputs[0].up,    SDL_SCANCODE_W)
+												DO_KEY(inputs[0].down,  SDL_SCANCODE_S)
+												DO_KEY(inputs[0].left,  SDL_SCANCODE_A)
+												DO_KEY(inputs[0].right, SDL_SCANCODE_D)
+												DO_KEY(inputs[0].a,     SDL_SCANCODE_O)
+												DO_KEY(inputs[0].b,     SDL_SCANCODE_P)
+												DO_KEY(inputs[0].c,     SDL_SCANCODE_LEFTBRACKET)
+												DO_KEY(inputs[0].start, SDL_SCANCODE_RETURN)
 
-													DO_KEY(inputs[0].up,    SDL_SCANCODE_W)
-													DO_KEY(inputs[0].down,  SDL_SCANCODE_S)
-													DO_KEY(inputs[0].left,  SDL_SCANCODE_A)
-													DO_KEY(inputs[0].right, SDL_SCANCODE_D)
-													DO_KEY(inputs[0].a,     SDL_SCANCODE_O)
-													DO_KEY(inputs[0].b,     SDL_SCANCODE_P)
-													DO_KEY(inputs[0].c,     SDL_SCANCODE_LEFTBRACKET)
-													DO_KEY(inputs[0].start, SDL_SCANCODE_RETURN)
+												DO_KEY(inputs[1].up,    SDL_SCANCODE_UP)
+												DO_KEY(inputs[1].down,  SDL_SCANCODE_DOWN)
+												DO_KEY(inputs[1].left,  SDL_SCANCODE_LEFT)
+												DO_KEY(inputs[1].right, SDL_SCANCODE_RIGHT)
+												DO_KEY(inputs[1].a,     SDL_SCANCODE_Z)
+												DO_KEY(inputs[1].b,     SDL_SCANCODE_X)
+												DO_KEY(inputs[1].c,     SDL_SCANCODE_C)
+												DO_KEY(inputs[1].start, SDL_SCANCODE_V)
 
-													DO_KEY(inputs[1].up,    SDL_SCANCODE_UP)
-													DO_KEY(inputs[1].down,  SDL_SCANCODE_DOWN)
-													DO_KEY(inputs[1].left,  SDL_SCANCODE_LEFT)
-													DO_KEY(inputs[1].right, SDL_SCANCODE_RIGHT)
-													DO_KEY(inputs[1].a,     SDL_SCANCODE_Z)
-													DO_KEY(inputs[1].b,     SDL_SCANCODE_X)
-													DO_KEY(inputs[1].c,     SDL_SCANCODE_C)
-													DO_KEY(inputs[1].start, SDL_SCANCODE_V)
+												DO_KEY(fast_forward,    SDL_SCANCODE_SPACE)
 
-													DO_KEY(fast_forward,    SDL_SCANCODE_SPACE)
+												#undef DO_KEY
 
-													#undef DO_KEY
+												case SDL_SCANCODE_TAB:
+													// Soft-reset console
+													if (pressed)
+														ClownMDEmu_Reset(&clownmdemu_state);
 
-													case SDL_SCANCODE_TAB:
-														// Soft-reset console
-														if (pressed)
-															ClownMDEmu_Reset(clownmdemu_state);
+													break;
 
-														break;
+												case SDL_SCANCODE_F5:
+													// Save save state
+													if (pressed)
+														clownmdemu_save_state = clownmdemu_state;
 
-													case SDL_SCANCODE_F5:
-														// Save save state
-														if (pressed)
-															clownmdemu_state[1] = clownmdemu_state[0];
+													break;
 
-														break;
+												case SDL_SCANCODE_F9:
+													// Load save state
+													if (pressed)
+														clownmdemu_state = clownmdemu_save_state;
 
-													case SDL_SCANCODE_F9:
-														// Load save state
-														if (pressed)
-															clownmdemu_state[0] = clownmdemu_state[1];
+													break;
 
-														break;
-
-													default:
-														break;
-												}
-
-												break;
+												default:
+													break;
 											}
+
+											break;
 										}
-									}
-
-									// Run the emulator for a frame
-									ClownMDEmu_Iterate(clownmdemu_state, ColourUpdatedCallback, ScanlineRenderedCallback, ReadInputCallback);
-
-									// Don't bother rendering if we're frame-skipping
-									if (!fast_forward || (++frameskip_counter & 3) == 0)
-									{
-										// Correct the aspect ratio of the rendered frame
-										// (256x224 and 320x240 should be the same width, but 320x224 and 320x240 should be different heights - this matches the behaviour of a real Mega Drive)
-										int renderer_width, renderer_height;
-										SDL_GetRendererOutputSize(renderer, &renderer_width, &renderer_height);
-
-										SDL_Rect destination_rect;
-
-										if ((unsigned int)renderer_width > renderer_height * 320 / current_screen_height)
-										{
-											destination_rect.w = renderer_height * 320 / current_screen_height;
-											destination_rect.h = renderer_height;
-										}
-										else
-										{
-											destination_rect.w = renderer_width;
-											destination_rect.h = renderer_width * current_screen_height / 320;
-										}
-
-										destination_rect.x = (renderer_width - destination_rect.w) / 2;
-										destination_rect.y = (renderer_height - destination_rect.h) / 2;
-
-										// Unlock the texture so that we can draw it
-										SDL_UnlockTexture(framebuffer_texture);
-
-										// Draw the rendered frame to the screen
-										SDL_RenderClear(renderer);
-										SDL_RenderCopy(renderer, framebuffer_texture, &(SDL_Rect){.x = 0, .y = 0, .w = current_screen_width, .h = current_screen_height}, &destination_rect);
-										SDL_RenderPresent(renderer);
-
-										// Lock the texture so that we can write to its pixels later
-										if (SDL_LockTexture(framebuffer_texture, NULL, (void*)&framebuffer_texture_pixels, &framebuffer_texture_pitch) < 0)
-											framebuffer_texture_pixels = NULL;
-
-										framebuffer_texture_pitch /= sizeof(Uint32);
-
-										// Framerate manager - run at roughly 60FPS
-										static Uint32 next_time;
-										const Uint32 current_time = SDL_GetTicks();
-
-										if (!SDL_TICKS_PASSED(current_time, next_time))
-											SDL_Delay(next_time - current_time);
-										else if (SDL_TICKS_PASSED(current_time, next_time + 100)) // If we're way past our deadline, then resync to the current tick instead of a fast-forwarding
-											next_time = current_time;
-
-										next_time += 1000 / 60;
 									}
 								}
+
+								// Run the emulator for a frame
+								ClownMDEmu_Iterate(&clownmdemu_state, ColourUpdatedCallback, ScanlineRenderedCallback, ReadInputCallback);
+
+								// Don't bother rendering if we're frame-skipping
+								if (!fast_forward || (++frameskip_counter & 3) == 0)
+								{
+									// Correct the aspect ratio of the rendered frame
+									// (256x224 and 320x240 should be the same width, but 320x224 and 320x240 should be different heights - this matches the behaviour of a real Mega Drive)
+									int renderer_width, renderer_height;
+									SDL_GetRendererOutputSize(renderer, &renderer_width, &renderer_height);
+
+									SDL_Rect destination_rect;
+
+									if ((unsigned int)renderer_width > renderer_height * 320 / current_screen_height)
+									{
+										destination_rect.w = renderer_height * 320 / current_screen_height;
+										destination_rect.h = renderer_height;
+									}
+									else
+									{
+										destination_rect.w = renderer_width;
+										destination_rect.h = renderer_width * current_screen_height / 320;
+									}
+
+									destination_rect.x = (renderer_width - destination_rect.w) / 2;
+									destination_rect.y = (renderer_height - destination_rect.h) / 2;
+
+									// Unlock the texture so that we can draw it
+									SDL_UnlockTexture(framebuffer_texture);
+
+									// Draw the rendered frame to the screen
+									SDL_RenderClear(renderer);
+									SDL_RenderCopy(renderer, framebuffer_texture, &(SDL_Rect){.x = 0, .y = 0, .w = current_screen_width, .h = current_screen_height}, &destination_rect);
+									SDL_RenderPresent(renderer);
+
+									// Lock the texture so that we can write to its pixels later
+									if (SDL_LockTexture(framebuffer_texture, NULL, (void*)&framebuffer_texture_pixels, &framebuffer_texture_pitch) < 0)
+										framebuffer_texture_pixels = NULL;
+
+									framebuffer_texture_pitch /= sizeof(Uint32);
+
+									// Framerate manager - run at roughly 60FPS
+									static Uint32 next_time;
+									const Uint32 current_time = SDL_GetTicks();
+
+									if (!SDL_TICKS_PASSED(current_time, next_time))
+										SDL_Delay(next_time - current_time);
+									else if (SDL_TICKS_PASSED(current_time, next_time + 100)) // If we're way past our deadline, then resync to the current tick instead of a fast-forwarding
+										next_time = current_time;
+
+									next_time += 1000 / 60;
+								}
 							}
-
-							FILE *state_file = fopen("state.bin", "wb");
-
-							if (state_file != NULL)
-							{
-								fwrite(clownmdemu_state, 1, clownmdemu_state_size, state_file);
-
-								fclose(state_file);
-							}
-
-							ClownMDEmu_Deinit(clownmdemu_state);
-
-							free(clownmdemu_state);
 						}
+
+						FILE *state_file = fopen("state.bin", "wb");
+
+						if (state_file != NULL)
+						{
+							fwrite(&clownmdemu_state, 1, sizeof(clownmdemu_state), state_file);
+
+							fclose(state_file);
+						}
+
+						ClownMDEmu_Deinit(&clownmdemu_state);
 
 						SDL_DestroyTexture(framebuffer_texture);
 					}
