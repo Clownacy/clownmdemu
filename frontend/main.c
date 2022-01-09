d909d3fac506fcfb1e3b2c7f4466526c6636305b
Clownacy <Clownacy@users.noreply.github.com>
2022-01-09 22:01:50 +0000

    Frontend: Fix SDL2-based error logging
    This is nice for seeing debug prints in Visual Studio's debug output
    console.

--- frontend/main.c
+++ frontend/main.c
@@ -38,10 +38,7 @@ static void PrintError(const char *fmt, ...)
 {
 	va_list args;
 	va_start(args, fmt);
-	fputs("Error: ", stderr);
-	vfprintf(stderr, fmt, args);
-	fputc('\n', stderr);
-//	SDL_LogMessageV(SDL_LOG_CATEGORY_ERROR, SDL_LOG_PRIORITY_ERROR, fmt, args);
+	SDL_LogMessageV(SDL_LOG_CATEGORY_ERROR, SDL_LOG_PRIORITY_ERROR, fmt, args);
 	va_end(args);
 }
 
@@ -185,6 +182,8 @@ static bool InitAudio(void)
 	}
 	else
 	{
+		SDL_LogSetPriority(SDL_LOG_CATEGORY_ERROR, SDL_LOG_PRIORITY_ERROR);
+
 		// Initialise audio backend
 		SDL_AudioSpec want, have;
 
