92ef66f5a63d1789abb2d7cfb6333ee4c53f2b29
Clownacy <Clownacy@users.noreply.github.com>
2022-01-22 22:16:46 +0000

    Frontend: Disable some debug code

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -855,7 +855,7 @@ int main(int argc, char **argv)
 				ImGui::NewFrame();
 
 				// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-				ImGui::ShowDemoWindow();
+				//ImGui::ShowDemoWindow();
 						
 				if (ImGui::BeginMainMenuBar())
 				{
