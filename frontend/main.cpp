f87098c6b90255c0b885ee06de80c64a11806163
Clownacy <Clownacy@users.noreply.github.com>
2022-01-28 22:30:00 +0000

    Frontend: Add PSG debugger
    Kinda rubbish, but it's better than nothing.

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -576,6 +576,7 @@ int main(int argc, char **argv)
 
 			bool vram_viewer = false;
 			bool cram_viewer = false;
+			bool psg_status = false;
 
 			bool dear_imgui_demo_window = false;
 
@@ -1223,6 +1224,9 @@ int main(int argc, char **argv)
 							if (ImGui::MenuItem("CRAM Viewer", NULL, cram_viewer))
 								cram_viewer = !cram_viewer;
 
+							if (ImGui::MenuItem("PSG Status", NULL, psg_status))
+								psg_status = !psg_status;
+
 							ImGui::EndMenu();
 						}
 
@@ -1592,6 +1596,76 @@ int main(int argc, char **argv)
 					}
 
 					ImGui::End();
+				}
+
+				if (psg_status)
+				{
+					if (ImGui::Begin("PSG Status", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+					{
+						if (ImGui::TreeNodeEx("Latched Command", ImGuiTreeNodeFlags_DefaultOpen))
+						{
+							if (clownmdemu_state.psg.latched_command.channel == 3)
+								ImGui::Text("Channel: Tone #%u", clownmdemu_state.psg.latched_command.channel);
+							else
+								ImGui::TextUnformatted("Channel: Noise");
+
+							ImGui::Text("Type: %s", clownmdemu_state.psg.latched_command.is_volume_command ? "Volume" : "Frequency");
+
+							ImGui::TreePop();
+						}
+
+						if (ImGui::TreeNodeEx("Channels", ImGuiTreeNodeFlags_DefaultOpen))
+						{
+							const unsigned long psg_clock = (tv_standard == CLOWNMDEMU_TV_STANDARD_PAL ? CLOWNMDEMU_MASTER_CLOCK_PAL : CLOWNMDEMU_MASTER_CLOCK_NTSC) / 15 / 16;
+
+							for (unsigned int i = 0; i < CC_COUNT_OF(clownmdemu_state.psg.tones); ++i)
+							{
+								char string_buffer[7 + 1];
+								SDL_snprintf(string_buffer, sizeof(string_buffer), "Tone #%d", i + 1);
+								if (ImGui::TreeNodeEx(string_buffer, ImGuiTreeNodeFlags_DefaultOpen))
+								{
+									ImGui::Text("Frequency: %u (%uHz)", clownmdemu_state.psg.tones[i].countdown_master, psg_clock / (clownmdemu_state.psg.tones[i].countdown_master + 1) / 2);
+
+									if (clownmdemu_state.psg.tones[i].attenuation == 15)
+										ImGui::TextUnformatted("Attenuation: 15 (Silent)");
+									else
+										ImGui::Text("Attenuation: %u (%udb)", clownmdemu_state.psg.tones[i].attenuation, clownmdemu_state.psg.tones[i].attenuation * 2);
+
+									ImGui::TreePop();
+								}
+							}
+
+							if (ImGui::TreeNodeEx("Noise", ImGuiTreeNodeFlags_DefaultOpen))
+							{
+								switch (clownmdemu_state.psg.noise.type)
+								{
+									case clownmdemu_state.psg.noise.PSG_NOISE_TYPE_PERIODIC:
+										ImGui::TextUnformatted("Type: Periodic Noise");
+										break;
+
+									case clownmdemu_state.psg.noise.PSG_NOISE_TYPE_WHITE:
+										ImGui::TextUnformatted("Type: White Noise");
+										break;
+								}
+
+								if (clownmdemu_state.psg.noise.frequency_mode == 3)
+									ImGui::TextUnformatted("Frequency Mode: Tone #3");
+								else
+									ImGui::Text("Frequency Mode: %u (%udb)", clownmdemu_state.psg.noise.frequency_mode, psg_clock / (0x10 << clownmdemu_state.psg.noise.frequency_mode) / 2);
+
+								if (clownmdemu_state.psg.noise.attenuation == 15)
+									ImGui::TextUnformatted("Attenuation: 15 (Silent)");
+								else
+									ImGui::Text("Attenuation: %u (%udb)", clownmdemu_state.psg.noise.attenuation, clownmdemu_state.psg.noise.attenuation * 2);
+
+								ImGui::TreePop();
+							}
+
+							ImGui::TreePop();
+						}
+
+						ImGui::End();
+					}
 				}
 
 				SDL_RenderClear(renderer);
