406f15993d4968f028df205dc3488d9d103d35b0
Clownacy <Clownacy@users.noreply.github.com>
2022-01-30 00:55:41 +0000

    Frontend: Enable SDL2 batching at all times
    Personally, I like to force SDL2 to use D3D11 instead of D3D9 by
    setting an environment variable, but it turns out that, if I do that,
    then SDL2 will disable its render batching for backwards
    compatibility reasons. Setting this hint prevents that.
    
    Normally if a user is setting an environment variable to force a
    specific rendering backend, then they're expected to set another
    environment variable to set this hint too, but I might as well just
    do it myself and save everyone else the hassle.

--- frontend/video.cpp
+++ frontend/video.cpp
@@ -27,6 +27,17 @@ bool InitVideo(void)
 		}
 		else
 		{
+			// Use batching even if the user forces a specific rendering backend (wtf SDL).
+			//
+			// Personally, I like to force SDL2 to use D3D11 instead of D3D9 by setting an environment
+			// variable, but it turns out that, if I do that, then SDL2 will disable its render batching
+			// for backwards compatibility reasons. Setting this hint prevents that.
+			//
+			// Normally if a user is setting an environment variable to force a specific rendering
+			// backend, then they're expected to set another environment variable to set this hint too,
+			// but I might as well just do it myself and save everyone else the hassle.
+			SDL_SetHint(SDL_HINT_RENDER_BATCHING, "1");
+
 			// Create renderer
 			renderer = SDL_CreateRenderer(window, -1, 0);
 
