db504ffe1e21bdf0530819657322634d19050f5a
Clownacy <Clownacy@users.noreply.github.com>
2022-02-02 13:12:40 +0000

    Frontend: Fix compilation with MinGW-w64
    Closes #1.

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -30,6 +30,10 @@
 #include "debug_psg.h"
 #include "debug_vdp.h"
 
+#ifndef M_PI
+#define M_PI 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+#endif
+
 typedef struct Input
 {
 	unsigned int bound_joypad;
