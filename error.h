f365a3692efe131f71ea6bd6752bb55d1a65a0eb
Clownacy <Clownacy@users.noreply.github.com>
2022-06-08 10:53:14 +0100

    Only use '__attribute__' on GCC 3.2 and later.
    In theory, if someone used an old-enough version of GCC to compile
    this, then they'd encounter an error stemming from '__attribute__'
    not being defined, because it didn't exist in that version.

--- error.h
+++ error.h
@@ -7,13 +7,16 @@
 extern "C" {
 #endif
 
-/* Prevent errors when __attribute__ is not supported. */
-#ifndef __GNUC__
-#define  __attribute__(x)
+/* Prevent errors when '__attribute__' is not supported. */
+/* GCC 3.2 is the earliest version of GCC of which I can find proof of supporting this. */
+#if defined(__GNUC__) && defined(__GNUC_MINOR__) && (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 2))
+#define GCC_ATTRIBUTE(x) __attribute__(x)
+#else
+#define GCC_ATTRIBUTE(x)
 #endif
 
 void SetErrorCallback(void (*error_callback)(const char *format, va_list arg));
-__attribute__((format(printf, 1, 2))) void PrintError(const char *format, ...);
+GCC_ATTRIBUTE((format(printf, 1, 2))) void PrintError(const char *format, ...);
 
 #ifdef __cplusplus
 }
