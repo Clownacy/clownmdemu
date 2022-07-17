d2f6c8c5f48437edc35f9636cdd491edb63c4dae
Clownacy <Clownacy@users.noreply.github.com>
2022-07-17 19:01:29 +0100

    Improve printf attribute stuff.

--- error.h
+++ error.h
@@ -7,16 +7,16 @@
 extern "C" {
 #endif
 
-/* Prevent errors when '__attribute__' is not supported. */
+/* Prevent errors when '__attribute__((format(printf, X, X)))' is not supported. */
 /* GCC 3.2 is the earliest version of GCC of which I can find proof of supporting this. */
 #if defined(__GNUC__) && defined(__GNUC_MINOR__) && (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 2))
-#define GCC_ATTRIBUTE(x) __attribute__(x)
+#define ATTRIBUTE_PRINTF(a, b) __attribute__((format(printf, a, b)))
 #else
-#define GCC_ATTRIBUTE(x)
+#define ATTRIBUTE_PRINTF(a, b)
 #endif
 
 void SetErrorCallback(void (*error_callback)(const char *format, va_list arg));
-GCC_ATTRIBUTE((format(printf, 1, 2))) void PrintError(const char *format, ...);
+ATTRIBUTE_PRINTF(1, 2) void PrintError(const char *format, ...);
 
 #ifdef __cplusplus
 }
