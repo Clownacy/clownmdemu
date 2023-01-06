dcd976ebb7e8b0e32ad0d852898ebaa008752a76
Clownacy <Clownacy@users.noreply.github.com>
2023-01-06 00:53:41 +0000

    Frontend: Correct printf specifiers for PSG debug

--- frontend/debug_psg.cpp
+++ frontend/debug_psg.cpp
@@ -17,7 +17,7 @@ void Debug_PSG(bool *open, ClownMDEmu *clownmdemu, ImFont *monospace_font)
 			if (clownmdemu->state->psg.latched_command.channel == 3)
 				ImGui::TextUnformatted("Channel: Noise");
 			else
-				ImGui::Text("Channel: Tone #%u", clownmdemu->state->psg.latched_command.channel + 1);
+				ImGui::Text("Channel: Tone #%" CC_PRIuFAST8, clownmdemu->state->psg.latched_command.channel + 1);
 
 			ImGui::TextUnformatted(clownmdemu->state->psg.latched_command.is_volume_command ? "Type:    Attenuation" : "Type:    Frequency");
 
@@ -29,23 +29,23 @@ void Debug_PSG(bool *open, ClownMDEmu *clownmdemu, ImFont *monospace_font)
 		// Channels.
 		if (ImGui::TreeNodeEx("Channels", ImGuiTreeNodeFlags_DefaultOpen))
 		{
-			const unsigned long psg_clock = (clownmdemu->configuration->general.tv_standard == CLOWNMDEMU_TV_STANDARD_PAL ? CLOWNMDEMU_MASTER_CLOCK_PAL : CLOWNMDEMU_MASTER_CLOCK_NTSC) / 15 / 16;
+			const cc_u32f psg_clock = (clownmdemu->configuration->general.tv_standard == CLOWNMDEMU_TV_STANDARD_PAL ? CLOWNMDEMU_MASTER_CLOCK_PAL : CLOWNMDEMU_MASTER_CLOCK_NTSC) / 15 / 16;
 
 			// Tone channels.
-			for (unsigned int i = 0; i < CC_COUNT_OF(clownmdemu->state->psg.tones); ++i)
+			for (cc_u8f i = 0; i < CC_COUNT_OF(clownmdemu->state->psg.tones); ++i)
 			{
 				char string_buffer[sizeof("Tone 0")];
-				SDL_snprintf(string_buffer, sizeof(string_buffer), "Tone %u", i + 1);
+				SDL_snprintf(string_buffer, sizeof(string_buffer), "Tone %" CC_PRIuFAST8, i + 1);
 				if (ImGui::TreeNodeEx(string_buffer, ImGuiTreeNodeFlags_DefaultOpen))
 				{
 					ImGui::PushFont(monospace_font);
 
-					ImGui::Text("Frequency: 0x%03X (%6luHz)", clownmdemu->state->psg.tones[i].countdown_master, psg_clock / (clownmdemu->state->psg.tones[i].countdown_master + 1) / 2);
+					ImGui::Text("Frequency: 0x%03" CC_PRIXFAST16 " (%6" CC_PRIuFAST32 "Hz)", clownmdemu->state->psg.tones[i].countdown_master, psg_clock / (clownmdemu->state->psg.tones[i].countdown_master + 1) / 2);
 
 					if (clownmdemu->state->psg.tones[i].attenuation == 15)
 						ImGui::TextUnformatted("Attenuation: 0xF (Mute)");
 					else
-						ImGui::Text("Attenuation: 0x%X (%2udb)", clownmdemu->state->psg.tones[i].attenuation, clownmdemu->state->psg.tones[i].attenuation * 2);
+						ImGui::Text("Attenuation: 0x%" CC_PRIXFAST8 " (%2" CC_PRIuFAST8 "db)", clownmdemu->state->psg.tones[i].attenuation, clownmdemu->state->psg.tones[i].attenuation * 2);
 
 					ImGui::PopFont();
 
@@ -72,12 +72,12 @@ void Debug_PSG(bool *open, ClownMDEmu *clownmdemu, ImFont *monospace_font)
 				if (clownmdemu->state->psg.noise.frequency_mode == 3)
 					ImGui::TextUnformatted("Frequency Mode: Tone #3");
 				else
-					ImGui::Text("Frequency Mode: %d (%4luHz)", clownmdemu->state->psg.noise.frequency_mode, psg_clock / (0x10 << clownmdemu->state->psg.noise.frequency_mode) / 2);
+					ImGui::Text("Frequency Mode: %" CC_PRIdFAST8 " (%4" CC_PRIuFAST32 "Hz)", clownmdemu->state->psg.noise.frequency_mode, psg_clock / (0x10 << clownmdemu->state->psg.noise.frequency_mode) / 2);
 
 				if (clownmdemu->state->psg.noise.attenuation == 15)
 					ImGui::TextUnformatted("Attenuation:  0xF (Mute)");
 				else
-					ImGui::Text("Attenuation:  0x%X (%2udb)", clownmdemu->state->psg.noise.attenuation, clownmdemu->state->psg.noise.attenuation * 2);
+					ImGui::Text("Attenuation:  0x%" CC_PRIXFAST8 " (%2" CC_PRIuFAST8 "db)", clownmdemu->state->psg.noise.attenuation, clownmdemu->state->psg.noise.attenuation * 2);
 
 				ImGui::PopFont();
 
